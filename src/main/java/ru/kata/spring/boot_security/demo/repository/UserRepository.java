8c05cd671c82e376296cf6c755584d5f9df27ec3
aleksey-komarov <zloy.komarik13@gmail.com>
2024-12-08 15:09:26 +0600

    1

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -3,9 +3,10 @@ package ru.kata.spring.boot_security.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.kata.spring.boot_security.demo.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("Select u from User u left join fetch u.roles where u.username=:username")
-    User findUsersByUsername(String username);
+    User findUsersByUsername(@Param("username") String username);
 }
